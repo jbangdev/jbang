4a8b169f92eb61933907f6eda0b7e3969661e678
Max Rydahl Andersen <max@xam.dk>
2019-12-25 16:42:36 +0100

    add initial homebrew formula

---
+++ src/main/scripts/jbang.rb
@@ -0,0 +1,14 @@
+class Jbang < Formula
+    desc "jbang"
+    homepage "https://github.com/maxandersen/jbang"
+    url "https://github.com/maxandersen/jbang/releases/download/v@project.version@/jbang-@project.version@.zip"
+    sha256 "@sha256@"
+  
+    #depends_on cask:"java"
+  
+    def install
+      libexec.install Dir["*"]
+      inreplace "#{libexec}/bin/jbang", /^jarPath=.*/, "jarPath=#{libexec}/bin/jbang.jar"
+      bin.install_symlink "#{libexec}/bin/jbang"
+    end
+  end
