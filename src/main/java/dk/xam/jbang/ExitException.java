7fb719f2757de74b1ca9affcc493170219e5116b
Max Rydahl Andersen <max@xam.dk>
2019-12-29 00:25:24 +0100

    [patch] enable debug

---
+++ src/main/java/dk/xam/jbang/ExitException.java
@@ -0,0 +1,6 @@
+package dk.xam.jbang;
+
+public class ExitException extends Throwable {
+    public ExitException(int status) {
+    }
+}
---
+++ src/test/java/dk/xam/jbang/TestMain.java
@@ -0,0 +1,4 @@
+package dk.xam.jbang;
+
+public class TestMain {
+}
