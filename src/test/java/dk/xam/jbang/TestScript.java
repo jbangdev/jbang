3606253ed187a880e37dc8ece07e1c6b60f139cf
Max Rydahl Andersen <max@xam.dk>
2019-12-31 00:50:40 +0100

    style: cleanup imports

--- src/test/java/dk/xam/jbang/TestScript.java
+++ src/test/java/dk/xam/jbang/TestScript.java
@@ -1,12 +1,12 @@
 package dk.xam.jbang;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class TestScript {
 
