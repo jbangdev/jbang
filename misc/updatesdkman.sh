56eb300f636df6fa046462024d5864ccd2ca9494
Max Rydahl Andersen <max@xam.dk>
2020-01-01 03:45:33 +0100

    ci: prep sdkman to point to sdkman

--- misc/updatesdkman.sh
+++ misc/updatesdkman.sh
@@ -8,6 +8,7 @@ echo "Updating jbang sdkman with version $jbang_version"
 echo ${SDKMAN_CONSUMER_KEY} | cut -c-5
 echo ${SDKMAN_CONSUMER_TOKEN} | cut -c-5
 
+echo "Release on sdkman"
 curl -X POST \
     -H "Consumer-Key: ${SDKMAN_CONSUMER_KEY}" \
     -H "Consumer-Token: ${SDKMAN_CONSUMER_TOKEN}" \
@@ -18,6 +19,7 @@ curl -X POST \
 
 ## Set existing Version as Default for Candidate
 
+echo "Set default version on sdkman"
 curl -X PUT \
     -H "Consumer-Key: ${SDKMAN_CONSUMER_KEY}" \
     -H "Consumer-Token: ${SDKMAN_CONSUMER_TOKEN}" \
@@ -27,10 +29,10 @@ curl -X PUT \
     https://vendors.sdkman.io/default
 
 ## Broadcast a Structured Message
-curl -X POST \
-    -H "Consumer-Key: ${SDKMAN_CONSUMER_KEY}" \
-    -H "Consumer-Token: ${SDKMAN_CONSUMER_TOKEN}" \
-    -H "Content-Type: application/json" \
-    -H "Accept: application/json" \
-    -d '{"candidate": "jbang", "version": "'${jbang_version}'", "hashtag": "jbang"}' \
-    https://vendors.sdkman.io/announce/struct
+#curl -X POST \
+#    -H "Consumer-Key: ${SDKMAN_CONSUMER_KEY}" \
+#    -H "Consumer-Token: ${SDKMAN_CONSUMER_TOKEN}" \
+#    -H "Content-Type: application/json" \
+#    -H "Accept: application/json" \
+#    -d '{"candidate": "jbang", "version": "'${jbang_version}'", "hashtag": "jbang"}' \
+#    https://vendors.sdkman.io/announce/struct
