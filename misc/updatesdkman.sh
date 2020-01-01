47f2f3e9f402a910d72d371006cbbdd814ebfd3e
Max Rydahl Andersen <max@xam.dk>
2020-01-01 03:23:29 +0100

    ci: Publishing to sdkman via github actions
    sdkman is awesome - lets make jbang available from there.

--- misc/updatesdkman.sh
+++ misc/updatesdkman.sh
@@ -5,21 +5,15 @@ jbang_version=`ls build/distributions/jbang-*.*.zip | sed -e 's/.*jbang-\(.*\).z
 echo "Updating jbang sdkman with version $jbang_version"
 
 
-echo ${SDKMAN_CONSUMER_KEY} ${SDKMAN_CONSUMER_TOKEN} ${jbang_version}
-#echo ${SDKMAN_CONSUMER_KEY} | cut -c-5
-#echo ${SDKMAN_CONSUMER_TOKEN} | cut -c-5
+echo ${SDKMAN_CONSUMER_KEY} | cut -c-5
+echo ${SDKMAN_CONSUMER_TOKEN} | cut -c-5
 
 curl -X POST \
     -H "Consumer-Key: ${SDKMAN_CONSUMER_KEY}" \
     -H "Consumer-Token: ${SDKMAN_CONSUMER_TOKEN}" \
     -H "Content-Type: application/json" \
-    -H "Accept: application/json" \$tools = Split-Path $MyInvocation.MyCommand.Definition
-$package = Split-Path $tools
-$jbang_home = Join-Path $package 'jbang-@projectVersion@'
-$jbang_bat = Join-Path $ant_home 'bin/jbang/bat'
-
-Uninstall-BinFile -Name 'jbang' -Path $jbang_bat
-    -d '{"candidate": "jbang", "version": "'${jbang_version}'", "url": "https://github.com/maxandersen/jbang/releases/download/v
+    -H "Accept: application/json" \
+    -d '{"candidate": "jbang", "version": "'${jbang_version}'", "url": "https://github.com/maxandersen/jbang/releases/download/v'${jbang_version}'/jbang-'${jbang_version}'.zip"}' \
     https://vendors.sdkman.io/release
 
 ## Set existing Version as Default for Candidate
