bfef7ef3d72096f68cc9375014374ec00a960624
Max Rydahl Andersen <max@xam.dk>
2020-01-17 00:00:20 +0100

    initial choco files

---
+++ src/main/scripts/choco/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$tools = Split-Path $MyInvocation.MyCommand.Definition
+$package = Split-Path $tools
+$jbang_home = Join-Path $package 'jbang-0.6.0.5'
+$jbang_bat = Join-Path $jbang_home 'bin/jbang.bat'
+
+Install-ChocolateyZipPackage `
+    -PackageName 'jbang' `
+    -Url 'https://github.com/maxandersen/jbang/releases/download/v0.6.0.5/jbang-0.6.0.5.zip' `
+    -Checksum 'e337cf5053091c893fef81141a79a6beb7d34777a65b25b11a379adc0d6c7203' `
+    -ChecksumType 'sha256' `
+    -UnzipLocation $package
+
+Install-BinFile -Name 'jbang' -Path $jbang_bat
+
+Update-SessionEnvironment
