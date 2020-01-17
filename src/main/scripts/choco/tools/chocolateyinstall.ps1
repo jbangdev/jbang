2a71d161895abc96383c3d5a4dd11032ada0c896
Max Rydahl Andersen <max@xam.dk>
2020-01-17 01:20:07 +0100

    initial choco metadata and enabled windows test run

--- src/main/scripts/choco/tools/chocolateyinstall.ps1
+++ src/main/scripts/choco/tools/chocolateyinstall.ps1
@@ -1,12 +1,12 @@
 ﻿$tools = Split-Path $MyInvocation.MyCommand.Definition
 $package = Split-Path $tools
-$jbang_home = Join-Path $package 'jbang-0.6.0.5'
+$jbang_home = Join-Path $package 'jbang-@projectVersion@'
 $jbang_bat = Join-Path $jbang_home 'bin/jbang.bat'
 
 Install-ChocolateyZipPackage `
     -PackageName 'jbang' `
-    -Url 'https://github.com/maxandersen/jbang/releases/download/v0.6.0.5/jbang-0.6.0.5.zip' `
-    -Checksum 'e337cf5053091c893fef81141a79a6beb7d34777a65b25b11a379adc0d6c7203' `
+    -Url 'https://github.com/maxandersen/jbang/releases/download/v@projectVersion@/jbang-@projectVersion@.zip' `
+    -Checksum '@sha256@' `
     -ChecksumType 'sha256' `
     -UnzipLocation $package
 
