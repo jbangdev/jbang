30bd3cca87a769d58909e09eaf4706995fbf08b7
Bruno Borges <bruno.borges@microsoft.com>
2020-01-17 02:03:16 -0800

    fixed samples

--- examples/webapps/jetty/jetty.jsh
+++ examples/webapps/jetty/jetty.jsh
@@ -1,10 +1,30 @@
 //usr/bin/env jbang "$0" "$@" ; exit $?
 
-//DEPS org.eclipse.jetty:jetty-server:9.4.25.v20191220
+//DEPS org.eclipse.jetty:jetty-server:9.4.25.v20191220,org.eclipse.jetty:jetty-servlet:9.4.25.v20191220
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import java.io.IOException;
+
+var servlet = new HttpServlet() {
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+            response.setStatus(HttpServletResponse.SC_OK);
+            response.setContentType("text/html");
+            response.setCharacterEncoding("utf-8");
+            response.getWriter().println("<h1>Hello from HelloServlet</h1>");
+        }
+};
+
+ServletHandler handler = new ServletHandler();
+handler.addServletWithMapping(new ServletHolder(servlet), "/*");
 
 var server = new Server(8080);
+server.setHandler(handler);
+
 server.start();
 server.dumpStdErr();
 server.join();
--- examples/webapps/spark/sparkjava.java
+++ examples/webapps/spark/sparkjava.java
@@ -14,59 +14,50 @@ public class sparkjava {
 
     void run() {
         var books = new HashMap();
-
-        var get = Spark.get;
-        var post = Spark.post;
         var random = new Random();
 
         Spark.setPort(8080);
-        Spark.staticFileLocation("/static");
 
-        get("/hello", (req, res) -> {
+        Spark.get("/hello", (req, res) -> {
             return "Hello Spark!";
         });
 
-        get("/json", "application/json", (req, res) -> {
+        Spark.get("/json", "application/json", (req, res) -> {
             var date = java.util.Calendar.getInstance().getTime();
-            var obj = {"name":"Nasven.js", "date":"${date}"};
-            return obj;
-}, function(model) JSON.stringify(model));
+            return String.format("{\"name\":\"jbang\", \"date\":\"%s\"}", date);
+        });
 
-// Below sample is based on the Books sample in README.md of SparkJava source code
+        Spark.post("/books", (request, response) -> {
+            var author = request.queryParams("author");
+            var title = request.queryParams("title");
+            var book = String.format("{\"author\":\"%s\", \"title\":\"%s\"}", author, title);
 
-// Creates a new book resource, will return the ID to the created resource
-// author and title are sent as query parameters e.g. /books?author=Foo&title=Bar
-post("/books", function(request, response) {
-  var author = request.queryParams("author");
-  var title = request.queryParams("title");
-  var book = {"author":"${author}", "title":"${title}"};
+            var id = random.nextInt(999999);
+            books.put(Integer.valueOf(id), book);
 
-  var id = random.nextInt(999999);
-  books.put(id.toString(), book);
+            response.status(201); // 201 Created
+            return id;
+        });
 
-  response.status(201); // 201 Created
-  return id;
-});
+        // Gets all available book resources (id's)
+        Spark.get("/books", (request, response) -> {
+            var ids = "";
+            for (var id : books.keySet()) {
+                ids += id + " ";
+            }
+            return ids;
+        });
 
-// Gets all available book resources (id's)
-get("/books", function(request, response) {
-  var ids = "";
-  for each (var id in books.keySet()) {
-    ids += id + " ";
-  }
-  return ids;
-});
-
-// Gets the book resource for the provided id
-get("/books/:id", function(request, response) {
-  var book = books.get(request.params(":id"));
-  if (book != null) {
-    return JSON.stringify(book);
-  } else {
-    response.status(404); // 404 Not found
-    return "Book not found";
-  }
-});
+        // Gets the book resource for the provided id
+        Spark.get("/books/:id", (request, response) -> { 
+            var book = books.get(request.params(":id"));
+            if (book != null) {
+                return book;
+            } else {
+                response.status(404); // 404 Not found
+                return "Book not found";
+            }
+        });
     }
 
 }
