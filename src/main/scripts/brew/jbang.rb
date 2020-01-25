6d9187a534cb35afd67141adbeb96f2910e46d51
Max Rydahl Andersen <max@xam.dk>
2020-01-25 23:10:04 +0100

    [patch] cleanup packaging and as much docs as possible in the metadata

---
+++ src/main/scripts/brew/jbang.rb
@@ -0,0 +1,24 @@
+class Jbang < Formula
+    desc "Unleash the power of Java for shell scripting"
+    homepage "https://github.com/maxandersen/jbang"
+    url "https://github.com/maxandersen/jbang/releases/download/v@projectVersion@/jbang-@projectVersion@.zip"
+    sha256 "@sha256@"
+
+    bottle :unneeded
+
+    keg_only :versioned_formula
+
+    #depends_on cask:"java"
+    depends_on :java => "1.8+"
+
+    def install
+      libexec.install Dir["*"]
+      inreplace "#{libexec}/bin/jbang", /^jarPath=.*/, "jarPath=#{libexec}/bin/jbang.jar"
+      bin.install_symlink "#{libexec}/bin/jbang"
+    end
+
+    test do
+        system "#{bin}/jbang", "--init=cli", "hello.java"
+        system "#{bin}/jbang", "hello.java", "Homebrew!"
+    end
+  end
