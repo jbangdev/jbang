5d928d79427d4a22ce94779ed8618f54db7faec8
Max Rydahl Andersen <max@xam.dk>
2020-02-21 01:42:34 +0000

    basic vertx.java

---
+++ examples/vertx.java
@@ -0,0 +1,82 @@
+//usr/bin/env jbang "$0" "$@" ; exit $?
+//DEPS io.vertx:vertx-core:3.8.5
+//DEPS io.vertx:vertx-web:3.8.5
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import io.vertx.ext.web.Router;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.function.Consumer;
+
+/*
+ * @author <a href="http://tfox.org">Tim Fox</a>
+ */
+public class vertx extends AbstractVerticle {
+
+  // Convenience method so you can run it in your IDE
+  public static void main(String[] args) {
+    runExample(".", "vertx", new VertxOptions().setClustered(false), null);
+  }
+
+  @Override
+  public void start() throws Exception {
+
+    Router router = Router.router(vertx);
+
+    router.route().handler(routingContext -> {
+      routingContext.response().putHeader("content-type", "text/html").end("Hello World!");
+    });
+
+    vertx.createHttpServer().requestHandler(router).listen(8080);
+  }
+
+  public static void runExample(String exampleDir, String verticleID, VertxOptions options, DeploymentOptions deploymentOptions) {
+    if (options == null) {
+      // Default parameter
+      options = new VertxOptions();
+    }
+    // Smart cwd detection
+
+    // Based on the current directory (.) and the desired directory (exampleDir), we try to compute the vertx.cwd
+    // directory:
+    try {
+      // We need to use the canonical file. Without the file name is .
+      File current = new File(".").getCanonicalFile();
+      if (exampleDir.startsWith(current.getName()) && !exampleDir.equals(current.getName())) {
+        exampleDir = exampleDir.substring(current.getName().length() + 1);
+      }
+    } catch (IOException e) {
+      // Ignore it.
+    }
+
+    System.setProperty("vertx.cwd", exampleDir);
+    Consumer<Vertx> runner = vertx -> {
+      try {
+        if (deploymentOptions != null) {
+          vertx.deployVerticle(verticleID, deploymentOptions);
+        } else {
+          vertx.deployVerticle(verticleID);
+        }
+      } catch (Throwable t) {
+        t.printStackTrace();
+      }
+    };
+    if (options.isClustered()) {
+      Vertx.clusteredVertx(options, res -> {
+        if (res.succeeded()) {
+          Vertx vertx = res.result();
+          runner.accept(vertx);
+        } else {
+          res.cause().printStackTrace();
+        }
+      });
+    } else {
+      Vertx vertx = Vertx.vertx(options);
+      runner.accept(vertx);
+    }
+  }
+}
