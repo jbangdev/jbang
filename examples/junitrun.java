f8da8e14c6151da72620bf6442be4ed8aba43b82
Max Rydahl Andersen <max@xam.dk>
2020-04-18 04:59:29 +0200

    rename so http run works

---
+++ examples/junitrun.java
@@ -0,0 +1,13 @@
+//usr/bin/env jbang "$0" "$@" ; exit $?
+//DEPS org.junit.platform:junit-platform-console-standalone:1.6.2
+
+import org.junit.platform.console.ConsoleLauncher;
+
+import static java.lang.System.*;
+
+public class junitrun {
+
+    public static void main(String... args) {
+        ConsoleLauncher.main(args);
+    }
+}
