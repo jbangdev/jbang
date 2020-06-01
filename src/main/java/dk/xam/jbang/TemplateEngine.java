b25337d750c50c7d65e1ecfca4ed38ca2d2fb79b
Max Rydahl Andersen <max@xam.dk>
2020-06-01 10:33:54 +0200

    Add support for trusted sources
    Why:
    
     * @gunnarmorling rickroll https://twitter.com/gunnarmorling/status/1267033700031778816
       made it clear it would be good if could add basic protection to not run arbitrary code
       on the internet.
    
    This change addreses the need by:
    
     * fix bug where error info not printed
     * before fetching url the url gets validated against trusted domains.
     * trusted sources are a list of patterns, if a match is found it is allowed;
       otherwise not.
     * if url not found jbang will print out instructions on how to add trust for that url.
    
    Fixes #116, #113

---
+++ src/main/java/dk/xam/jbang/TemplateEngine.java
@@ -0,0 +1,79 @@
+package dk.xam.jbang;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
+
+import io.quarkus.qute.Engine;
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateLocator;
+import io.quarkus.qute.Variant;
+
+public class TemplateEngine {
+
+	final Engine engine;
+
+	TemplateEngine() {
+		engine = Engine.builder().addDefaults().addLocator(this::locate).build();
+	}
+
+	private URL locatePath(String path) {
+		ClassLoader cl = Thread.currentThread().getContextClassLoader();
+		if (cl == null) {
+			cl = Main.class.getClassLoader();
+		}
+		return cl.getResource(path);
+	}
+
+	/**
+	 * @param path
+	 * @return the optional reader
+	 */
+	private Optional<TemplateLocator.TemplateLocation> locate(String path) {
+		URL resource = null;
+		String basePath = "";
+		String templatePath = basePath + path;
+		// LOGGER.debugf("Locate template for %s", templatePath);
+		resource = locatePath(templatePath);
+
+		if (resource != null) {
+			return Optional.of(new ResourceTemplateLocation(resource));
+		}
+		return Optional.empty();
+	}
+
+	public Template getTemplate(String templateName) {
+		return engine.getTemplate(templateName);
+	}
+
+	static class ResourceTemplateLocation implements TemplateLocator.TemplateLocation {
+
+		private final URL resource;
+		@SuppressWarnings("OptionalUsedAsFieldOrParameterType")
+		private Optional<Variant> variant = null;
+
+		public ResourceTemplateLocation(URL resource) {
+			this.resource = resource;
+			this.variant = null;
+		}
+
+		@Override
+		public Reader read() {
+			try {
+				return new InputStreamReader(resource.openStream(), StandardCharsets.UTF_8);
+			} catch (IOException e) {
+				return null;
+			}
+		}
+
+		@Override
+		public Optional<Variant> getVariant() {
+			return variant;
+		}
+
+	}
+
+}
---
+++ src/main/java/dk/xam/jbang/TrustedSources.java
@@ -0,0 +1,195 @@
+package dk.xam.jbang;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+import com.google.gson.Gson;
+
+public class TrustedSources {
+
+	final static Pattern rLocalhost = Pattern.compile("(?i)^localhost(:\\d+)?$");
+	final static Pattern r127 = Pattern.compile("(?i)^127.0.0.1(:\\d+)?$");
+
+	String trustedSources[];
+
+	public TrustedSources(String[] trustedSources) {
+		this.trustedSources = trustedSources;
+	}
+
+	public static TrustedSources load(Path toPath) throws IOException {
+		String rawtd = Util.readString(toPath);
+		Gson parser = new Gson();
+		String[] strings = parser.fromJson(rawtd, String[].class);
+		if (strings == null) {
+			throw new IllegalStateException("Could not parse " + toPath);
+		} else {
+			return new TrustedSources(strings);
+		}
+	}
+
+	private boolean isLocalhostAuthority(URI uri) {
+		return uri.getScheme().equals("file") || rLocalhost.matcher(uri.getAuthority()).matches()
+				|| r127.matcher(uri.getAuthority()).matches();
+	}
+
+	public String[] getTrustedSources() {
+		return trustedSources;
+	}
+
+	/**
+	 * Check whether a url like https://www.microsoft.com matches the list of
+	 * trusted sources.
+	 *
+	 * - Schemes must match - There's no subdomain matching. For example
+	 * https://jbang.dev doesn't match https://www.jbang.dev - Star matches all
+	 * subdomains. For example https://*.jbang.dev matches https://www.jbang.dev and
+	 * https://foo.bar.jbangdev.com
+	 */
+	public boolean isURLTrusted(URI url) throws URISyntaxException {
+		if (isLocalhostAuthority(url)) {
+			return true;
+		}
+
+		final String domain = String.format("%s://%s", url.getScheme(), url.getAuthority());
+
+		for (String trustedSource : trustedSources) {
+			if ("*".equals(trustedSource)) {
+				return true;
+			}
+
+			if (domain.equals(trustedSource)) {
+				return true;
+			}
+
+			URI parsedTrustedSource;
+			if (trustedSource.startsWith("https://")) {
+				parsedTrustedSource = new URI(trustedSource);
+				if (!url.getScheme().equals(parsedTrustedSource.getScheme())) {
+					continue;
+				}
+			} else {
+				parsedTrustedSource = new URI("https://" + trustedSource);
+			}
+
+			if (url.getAuthority().equals(parsedTrustedSource.getAuthority())) {
+				if (pathMatches(url.getPath(), parsedTrustedSource.getPath())) {
+					return true;
+				} else {
+					continue;
+				}
+			}
+
+			if (trustedSource.contains("*")) {
+
+				String[] reversedAuthoritySegments = reverse(url.getAuthority().split("\\."));
+
+				String[] reversedTrustedSourceAuthoritySegments = reverse(
+						parsedTrustedSource.getAuthority().split("\\."));
+
+				boolean ruleIsSmaller = reversedTrustedSourceAuthoritySegments.length < reversedAuthoritySegments.length;
+				boolean ruleHasStarAtEnd = reversedTrustedSourceAuthoritySegments[reversedTrustedSourceAuthoritySegments.length
+						- 1].equals("*");
+				if (ruleIsSmaller && ruleHasStarAtEnd) {
+					reversedAuthoritySegments = Arrays.copyOfRange(reversedAuthoritySegments, 0,
+							reversedTrustedSourceAuthoritySegments.length);
+				}
+
+				boolean authorityMatches = true;
+				for (int i = 0; i < reversedAuthoritySegments.length; i++) {
+					String val = reversedAuthoritySegments[i];
+					String elementRule = reversedTrustedSourceAuthoritySegments[i];
+					if (elementRule.equals("*")
+							|| val.equals(elementRule)) {
+						// they match
+					} else {
+						authorityMatches = false;
+						break;
+					}
+				}
+
+				if (authorityMatches && pathMatches(url.getPath(), parsedTrustedSource.getPath())) {
+					return true;
+				}
+			}
+
+		}
+		return false;
+	}
+
+	boolean pathMatches(String open, String rule) {
+		if ("/".equals(rule)) {
+			return true;
+		}
+
+		if (rule.endsWith("/")) {
+			rule = rule.substring(0, rule.length() - 1);
+		}
+
+		String[] openSegments = open.split("/");
+		String[] ruleSegments = rule.split("/");
+		for (int i = 0; i < ruleSegments.length; i++) {
+			if (openSegments.length - 1 < i || !ruleSegments[i].equals(openSegments[i])) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 * in place reverse of array
+	 * 
+	 * @param array array to reverse
+	 * @param <T>
+	 * @return reversed array
+	 */
+	static <T> T[] reverse(T[] array) {
+		int i, k;
+		T temp;
+		for (i = 0; i < array.length / 2; i++) {
+			temp = array[i];
+			array[i] = array[array.length - i - 1];
+			array[array.length - i - 1] = temp;
+		}
+		return array;
+	}
+
+	String getJSon(Collection<String> rules) {
+		String trustedsources = Settings.getTemplateEngine()
+										.getTemplate("trusted-sources.json.qute")
+										.data("trustedsources", rules)
+										.render();
+		return trustedsources;
+	}
+
+	public void add(List<String> trust, File storage) {
+
+		Util.info("Adding " + trust + " to " + storage);
+
+		Set<String> newrules = new LinkedHashSet<String>();
+
+		newrules.addAll(Arrays.asList(trustedSources));
+
+		if (newrules.addAll(trust)) {
+			String newsources = getJSon(newrules);
+			try {
+				Util.writeString(storage.toPath(), newsources);
+			} catch (IOException e) {
+				throw new ExitException(2, "Error when writing to " + storage, e);
+			}
+			trustedSources = newrules.toArray(new String[0]);
+		} else {
+			Util.warnMsg("Already trusted source(s). No changes made.");
+		}
+
+	}
+}
