532879820bc57f0ec8d9065da87b97c381c13d9d
Max Rydahl Andersen <max@xam.dk>
2020-06-22 11:36:03 +0200

    basic plantuml

---
+++ examples/plantuml.java
@@ -0,0 +1,13 @@
+//usr/bin/env jbang "$0" "$@" ; exit $?
+//DEPS net.sourceforge.plantuml:plantuml:1.2020.11
+import net.sourceforge.plantuml.Run;
+
+import java.io.IOException;
+
+class plantuml {
+
+    public static void main(String... args) throws IOException, InterruptedException {
+        Run.main(args);
+    }
+
+}
