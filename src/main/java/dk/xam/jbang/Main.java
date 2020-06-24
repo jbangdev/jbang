9d35f9c3f15b6c78b064a0520b9b5b99bf7dc3d6
Tako Schotanus <tako@codejive.org>
2020-06-24 17:49:50 +0200

    Made the "run" command the default if no subcommand is specified

--- src/main/java/dk/xam/jbang/Main.java
+++ src/main/java/dk/xam/jbang/Main.java
@@ -131,8 +131,35 @@ public class Main extends JbangBaseCommand {
 	}
 
 	public static void main(String... args) {
-		int exitcode = getCommandLine().execute(args);
+		CommandLine cli = getCommandLine();
+		args = handleDefaultRun(cli.getCommandSpec(), args);
+		int exitcode = cli.execute(args);
 		System.exit(exitcode);
+	}
+
+	private static String[] handleDefaultRun(CommandSpec spec, String[] args) {
+		List<String> leadingOpts = new ArrayList<>();
+		List<String> remainingArgs = new ArrayList<>();
+		boolean foundParam = false;
+		for (String arg : args) {
+			if (!arg.startsWith("-")) {
+				foundParam = true;
+			}
+			if (foundParam) {
+				remainingArgs.add(arg);
+			} else {
+				leadingOpts.add(arg);
+			}
+		}
+		// Check if we have a parameter and it's not the same as any of the subcommand names
+		if (!remainingArgs.isEmpty() && !spec.subcommands().keySet().contains(remainingArgs.get(0))) {
+			List<String> result = new ArrayList<>();
+			result.add("run");
+			result.addAll(leadingOpts);
+			result.addAll(remainingArgs);
+			args = result.toArray(args);
+		}
+		return args;
 	}
 
 	static CommandLine getCommandLine() {
