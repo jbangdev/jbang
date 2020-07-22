1da81d901f9e09dff9aea051d10040404686f35d
Tako Schotanus <tako@codejive.org>
2020-07-22 16:07:02 +0200

    Implemented bootstrapping of Jbang when Java is not available
    Fixes #197

--- src/main/scripts/jbang.cmd
+++ src/main/scripts/jbang.cmd
@@ -1,16 +1,13 @@
 @echo off
 SETLOCAL ENABLEEXTENSIONS
 
+rem The Java version to install when it's not installed on the system yet
+set javaVersion=11
+set os=windows
+set arch=x64
+
 rem resolve application jar path from script location and convert to windows path when using cygwin
 set jarPath=%~dp0jbang.jar
-
-rem prefer JAVA instead of PATH to resolve `java` location
-rem if [[ -z "$JAVA_HOME" ]]; then JAVA_EXEC="java"; else JAVA_EXEC="$JAVA_HOME/bin/java"; fi
-if "%JAVA_HOME%"=="" (
-  set JAVA_EXEC=java.exe
-) else (
-  set JAVA_EXEC="%JAVA_HOME%\bin\java.exe"
-)
 
 rem expose the name of the script being run to the script itself
 set JBANG_FILE="$1"
@@ -20,9 +17,35 @@ set OUTPUT=
 
 rem create TDIR based on jbang_dir in case it is missing to have a folder
 rem we need to be able to write to anyway
-IF "%JBANG_DIR%"=="" (set TDIR=%userprofile%\.jbang) ELSE (set TDIR=%JBANG_DIR%\.jbang)
+IF "%JBANG_DIR%"=="" (set JBDIR=%userprofile%\.jbang) ELSE (set JBDIR=%JBANG_DIR%)
+IF "%JBANG_CACHE_DIR%"=="" (set TDIR=%JBDIR%\cache) ELSE (set TDIR=%JBANG_CACHE_DIR%)
 
-IF NOT EXIST "%TDIR%" ( mkdir "%TDIR%")
+IF NOT EXIST "%TDIR%\jdks" ( mkdir "%TDIR%\jdks" )
+
+rem prefer JAVA instead of PATH to resolve `java` location
+rem if [[ -z "$JAVA_HOME" ]]; then JAVA_EXEC="java"; else JAVA_EXEC="$JAVA_HOME/bin/java"; fi
+if "%JAVA_HOME%"=="" (
+  where javac
+  if %errorlevel% neq 0 (
+    set JAVA_EXEC=java.exe
+  ) else (
+    IF NOT EXIST "%TDIR%\jdks\%javaVersion%" (
+      echo "Downloading JDK %javaVersion%..."
+      set url=https://api.adoptopenjdk.net/v3/binary/latest/%javaVersion%/ga/%os%/%arch%/jdk/hotspot/normal/adoptopenjdk
+      powershell -Command "Invoke-WebRequest %url%" -OutFile "%TDIR%\bootstrap-jdk.zip" -Force
+      echo "Installing JDK %javaVersion%..."
+      del /s /q "%TDIR%\jdks\%javaVersion%.tmp"
+      mkdir "%TDIR%\jdks\%javaVersion%.tmp"
+      powershell -Command "Expand-Archive" "%TDIR%\bootstrap-jdk.zip" "%TDIR%\jdks\%javaVersion%.tmp"
+      rem TODO move contents of unpacked contents one folder up
+      ren "%TDIR%\jdks\%javaVersion%.tmp" "%TDIR%\jdks\%javaVersion%"
+    )
+    set JAVA_HOME="%TDIR%\jdks\%javaVersion%"
+    set JAVA_EXEC="%JAVA_HOME%\bin\java.exe"
+  )
+) else (
+  set JAVA_EXEC="%JAVA_HOME%\bin\java.exe"
+)
 
 set tmpfile=%TDIR%\%RANDOM%.tmp
 rem execute jbang and pipe to temporary random file
@@ -31,7 +54,6 @@ set ERROR=%ERRORLEVEL%
 rem catch errorlevel straight after; rem or FOR /F swallow would have swallowed the errorlevel
 
 IF %ERROR% NEQ 0 (
-
     del ""%tmpfile%""
     exit /b %ERROR%
 )
