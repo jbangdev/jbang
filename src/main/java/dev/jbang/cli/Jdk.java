aabc9077ca80f434e3bee4bf916b394c288bca2b
Tako Schotanus <tako@codejive.org>
2020-09-20 17:33:40 +0200

    feat: Added `jdk default`, `jdk home` and `jdk java-env` commands
    These commands were added to facilitate the use of the Jbang's JDKs
    by the user. This way it's not necessary to install additional JDKs
    if we want to use Java and have previously let Jbang install a JDK.
    `jdk default` set the given JDK as "current".
    `jdk home` returns the path of the current or the given JDK.
    `jdk java-env` returns the shell commands to make the current or
    the given JDK usable in the user's shell.

--- src/main/java/dev/jbang/cli/Jdk.java
+++ src/main/java/dev/jbang/cli/Jdk.java
@@ -1,9 +1,12 @@
 package dev.jbang.cli;
 
 import java.io.IOException;
+import java.io.PrintStream;
 import java.io.PrintWriter;
+import java.nio.file.Path;
 
 import dev.jbang.JdkManager;
+import dev.jbang.Settings;
 import dev.jbang.Util;
 
 import picocli.CommandLine;
@@ -25,14 +28,21 @@ public class Jdk {
 		} else {
 			Util.infoMsg("JDK " + version + " is already installed");
 		}
-		return CommandLine.ExitCode.SOFTWARE;
+		return CommandLine.ExitCode.OK;
 	}
 
 	@CommandLine.Command(name = "list", description = "Lists installed JDKs.")
-	public Integer list() throws IOException {
+	public Integer list() {
+		int v = JdkManager.getDefaultJdk();
 		PrintWriter err = spec.commandLine().getErr();
-		JdkManager.listInstalledJdks().forEach(jdk -> err.println(jdk));
-		return CommandLine.ExitCode.SOFTWARE;
+		JdkManager.listInstalledJdks().forEach(jdk -> {
+			err.print(jdk);
+			if (jdk == v) {
+				err.print(" *");
+			}
+			err.println();
+		});
+		return CommandLine.ExitCode.OK;
 	}
 
 	@CommandLine.Command(name = "uninstall", description = "Uninstalls an existing JDK.")
@@ -44,6 +54,75 @@ public class Jdk {
 		} else {
 			Util.infoMsg("JDK " + version + " is not installed");
 		}
-		return CommandLine.ExitCode.SOFTWARE;
+		return CommandLine.ExitCode.OK;
+	}
+
+	@CommandLine.Command(name = "home", description = "Returns the folder where the given JDK is installed.")
+	public Integer home(
+			@CommandLine.Parameters(paramLabel = "version", index = "0", description = "The version of the JDK to select", arity = "0..1") Integer version) {
+		Path home = getJdkPath(version);
+		System.out.println("echo " + home);
+		return CommandLine.ExitCode.OK;
+	}
+
+	@CommandLine.Command(name = "java-env", description = "Returns the folder where the given JDK is installed.")
+	public Integer javaEnv(
+			@CommandLine.Parameters(paramLabel = "version", index = "0", description = "The version of the JDK to select", arity = "0..1") Integer version) {
+		Path home = getJdkPath(version);
+		if (home != null) {
+			PrintStream out = System.err; // TODO set this to System.out when #336 gets merged
+			if (Util.isWindows()) {
+				out.println("set PATH=" + home + "\\bin;%PATH%");
+				out.println("set JAVA_HOME=" + home);
+				out.println("rem Copy & paste the above commands in your CMD window or add");
+				out.println("rem them to your Environment Variables in the System Settings.");
+			} else {
+				out.println("export PATH=\"" + home + "/bin:$PATH\"");
+				out.println("export JAVA_HOME=\"" + home + "\"");
+				out.println("# Run this command to configure your shell:");
+				out.print("# eval $(jbang jdk java-env");
+				if (version != null) {
+					out.print(" " + version);
+				}
+				out.println(")");
+			}
+		}
+		return CommandLine.ExitCode.OK; // TODO set this to EXIT_PRINT when #336 gets merged
+	}
+
+	private Path getJdkPath(Integer version) {
+		Path home;
+		if (version == null) {
+			int v = JdkManager.getDefaultJdk();
+			if (v < 0) {
+				Util.infoMsg("No default JDK set, use 'jbang jdk default <version>' to set one.");
+				return null;
+			}
+			home = Settings.getCurrentJdkDir();
+		} else {
+			home = JdkManager.getInstalledJdk(version);
+		}
+		return home;
+	}
+
+	@CommandLine.Command(name = "default", description = "Sets the default JDK to be used by Jbang.")
+	public Integer defaultJdk(
+			@CommandLine.Parameters(paramLabel = "version", index = "0", description = "The version of the JDK to select", arity = "0..1") Integer version)
+			throws IOException {
+		int v = JdkManager.getDefaultJdk();
+		if (version != null) {
+			if (v != version) {
+				JdkManager.setDefaultJdk(version);
+			} else {
+				Util.infoMsg("Default JDK already set to " + v);
+			}
+		} else {
+			if (v < 0) {
+				Util.infoMsg("No default JDK set, use 'jbang jdk default <version>' to set one.");
+			} else {
+				Util.infoMsg("Default JDK is currently set to " + v);
+			}
+		}
+		return CommandLine.ExitCode.OK;
 	}
 }
