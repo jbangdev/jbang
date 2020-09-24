dea58992e5ec0bcbd6c797b74ded32dc363645d3
Max Rydahl Andersen <max@xam.dk>
2020-09-25 00:44:20 +0200

    fix: os.detected.jfxname now properly resolved
    Fixes #353
    
    [minor]

--- src/main/java/dev/jbang/PropertiesValueResolver.java
+++ src/main/java/dev/jbang/PropertiesValueResolver.java
@@ -98,7 +98,7 @@ public class PropertiesValueResolver {
 						continue;
 					}
 					// First check for system property, then env variable
-					String val = (String) properties.get(name);
+					String val = (String) properties.getProperty(name);
 					if (val == null && name.startsWith("env."))
 						val = System.getenv(name.substring(4));
 
