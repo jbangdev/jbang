b92a9e06b549b77e6624dfc5820e9b94b6136f06
Max Rydahl Andersen <max@xam.dk>
2021-01-04 09:30:10 +0100

    feat: add -V/--version for basic version info. Fix #601

--- src/main/java/dev/jbang/cli/BaseCommand.java
+++ src/main/java/dev/jbang/cli/BaseCommand.java
@@ -26,6 +26,10 @@ public abstract class BaseCommand implements Callable<Integer> {
 	@CommandLine.Spec
 	CommandLine.Model.CommandSpec spec;
 
+	@CommandLine.Option(names = { "-V",
+			"--version" }, versionHelp = true, description = "Display version info (use `jbang --verbose version` for more details)")
+	boolean versionRequested;
+
 	@CommandLine.Option(names = { "-h", "--help" }, usageHelp = true, description = "Display help/info")
 	boolean helpRequested;
 
