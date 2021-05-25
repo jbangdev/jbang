a3358c86cbdfcaeca9de8bd16b792b685f57493a
Tako Schotanus <tako@codejive.org>
2021-05-25 16:16:25 +0200

    fix: No longer unnecessarily recompiles
    The check to determine if a script had to be recompiled
    because of a mismatch between the JDK version that was
    used to compile the JAR and the available JDK was not
    entirely correct and was causing unnecessary recompiles.

--- src/main/java/dev/jbang/cli/BaseBuildCommand.java
+++ src/main/java/dev/jbang/cli/BaseBuildCommand.java
@@ -103,7 +103,8 @@ public abstract class BaseBuildCommand extends BaseScriptDepsCommand {
 			JarSource jarSrc = src.asJarSource();
 			if (jarSrc == null
 					|| !jarSrc.isUpToDate()
-					|| JavaUtil.javaVersion(requestedJavaVersion) < JavaUtil.javaVersion(jarSrc.getJavaVersion())) {
+					|| JavaUtil.javaVersion(requestedJavaVersion) < JavaUtil.minRequestedVersion(
+							jarSrc.getJavaVersion())) {
 				buildRequired = true;
 			} else {
 				result = ctx.importJarMetadataFor(jarSrc);
