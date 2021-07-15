5877729c7df607258254f291e73377855f900a73
Max Rydahl Andersen <gitkraken@xam.dk>
2021-07-15 23:29:44 +0200

    [minor] relase

---
+++ src/main/scripts/choco/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$tools = Split-Path $MyInvocation.MyCommand.Definition
+$package = Split-Path $tools
+$jbang_home = Join-Path $package 'jbang-@projectVersion@'
+$jbang_bat = Join-Path $jbang_home 'bin/jbang.cmd'
+
+Install-ChocolateyZipPackage `
+    -PackageName 'jbang' `
+    -Url 'https://github.com/jbangdev/jbang/releases/download/v@projectVersion@/jbang-@projectVersion@.zip' `
+    -Checksum '@sha256@' `
+    -ChecksumType 'sha256' `
+    -UnzipLocation $package
+
+Install-BinFile -Name 'jbang' -Path $jbang_bat
---
+++ src/main/scripts/choco/tools/chocolateyuninstall.ps1
@@ -0,0 +1,6 @@
+﻿$tools = Split-Path $MyInvocation.MyCommand.Definition
+$package = Split-Path $tools
+$jbang_home = Join-Path $package 'jbang-@projectVersion@'
+$jbang_bat = Join-Path $jbang_home 'bin/jbang.cmd'
+
+Uninstall-BinFile -Name 'jbang' -Path $jbang_bat
