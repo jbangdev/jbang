6173f7991caadd78b3251169b097be775ec94687
Max Rydahl Andersen <max@xam.dk>
2021-11-28 12:36:29 +0100

    chore: enable code coverage (#448)
    * chore: enable code coverage
    
    * chore: upload to codecov

--- itests/itests.sh
+++ itests/itests.sh
@@ -3,4 +3,4 @@ export PATH=`realpath ../build/install/jbang/bin`:$PATH
 ## github seem to be setting this thus trying to ensure it does not affect it here.
 unset JAVA_TOOL_OPTIONS
 
-jbang ./karate.java -o ../build/karate *.feature
+jbang --javaagent=org.jacoco:org.jacoco.agent:0.8.7:runtime=destfile=../build/jacoco/test.exec ./karate.java -o ../build/karate *.feature
