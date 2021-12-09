9343d7b77877e86375dc3f434c2ce5249d312f31
linux_china <libing.chen@gmail.com>
2021-12-08 22:16:00 -0800

    GroovyScriptSource

---
+++ src/main/java/dev/jbang/source/GroovyScriptSource.java
@@ -0,0 +1,60 @@
+package dev.jbang.source;
+
+import static dev.jbang.net.GroovyManager.resolveInGroovyHome;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+import org.jboss.jandex.ClassInfo;
+
+import dev.jbang.cli.BaseBuildCommand;
+import dev.jbang.net.GroovyManager;
+
+public class GroovyScriptSource extends ScriptSource {
+
+	protected GroovyScriptSource(ResourceRef script, Function<String, String> replaceProperties) {
+		super(script, replaceProperties);
+	}
+
+	@Override
+	public List<String> getCompileOptions() {
+		return Collections.emptyList();
+	}
+
+	@Override
+	public String getCompilerBinary(String requestedJavaVersion) {
+		return resolveInGroovyHome("groovyc", getGroovyVersion());
+	}
+
+	@Override
+	public Predicate<ClassInfo> getMainFinder() {
+		return pubClass -> pubClass.method("main", BaseBuildCommand.STRINGARRAYTYPE) != null
+				|| pubClass.method("main") != null;
+	}
+
+	@Override
+	public List<String> getAllDependencies() {
+		final List<String> allDependencies = super.getAllDependencies();
+		final String groovyVersion = getGroovyVersion();
+		if (groovyVersion.startsWith("4.")) {
+			allDependencies.add("org.apache.groovy:groovy:" + groovyVersion);
+		} else {
+			allDependencies.add("org.codehaus.groovy:groovy:" + groovyVersion);
+		}
+		return allDependencies;
+	}
+
+	@Override
+	protected String getMainExtension() {
+		return ".groovy";
+	}
+
+	public String getGroovyVersion() {
+		return collectAll((s) -> collectOptions("GROOVY"))
+															.stream()
+															.findFirst()
+															.orElse(GroovyManager.DEFAULT_GROOVY_VERSION);
+	}
+}
