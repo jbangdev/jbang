c4103b9c68dc30e56ba9913e364c3ca5f945c5c4
Tako Schotanus <tako@codejive.org>
2022-03-10 20:11:52 +0100

    refactor: extacted code for single integration execution
    The code that runs a single integration is now sepearate and its input
    and ouput have been turned into serializable objects. This prepares
    for the next step where those integrations can be run externally.

--- src/main/java/dev/jbang/spi/IntegrationManager.java
+++ src/main/java/dev/jbang/spi/IntegrationManager.java
@@ -20,10 +20,13 @@ import java.nio.file.Path;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import javax.annotation.Nonnull;
+
 import dev.jbang.cli.ExitException;
 import dev.jbang.dependencies.ArtifactInfo;
 import dev.jbang.dependencies.MavenRepo;
 import dev.jbang.source.Source;
+import dev.jbang.source.SourceSet;
 import dev.jbang.util.Util;
 
 /**
@@ -41,101 +44,37 @@ public class IntegrationManager {
 	 * <p>
 	 * If an integration point created a native image it returns the resulting
 	 * image.
-	 *
-	 *
-	 * @param repositories
-	 * @param artifacts
-	 * @param tmpJarDir
-	 * @param pomPath
-	 * @param source
-	 * @param nativeRequested
-	 * @return
 	 */
-	public static IntegrationResult runIntegration(List<MavenRepo> repositories, List<ArtifactInfo> artifacts,
-			Path tmpJarDir,
-			Path pomPath, Source source,
+	public static IntegrationResult runIntegrations(SourceSet ss, Path tmpJarDir, Path pomPath,
 			boolean nativeRequested) {
-		URL[] urls = artifacts.stream().map(s -> {
-			try {
-				return s.getFile().toURI().toURL();
-			} catch (MalformedURLException e) {
-				throw new RuntimeException(e);
-			}
-		}).toArray(URL[]::new);
-		List<String> comments = source.getLines().stream().filter(s -> s.startsWith("//")).collect(Collectors.toList());
-		URLClassLoader integrationCl = new URLClassLoader(urls);
+		IntegrationResult result = new IntegrationResult(null, null, null);
+		Source source = ss.getMainSource();
+
+		LinkedHashMap<String, String> repos = new LinkedHashMap<>();
+		LinkedHashMap<String, Path> deps = new LinkedHashMap<>();
+		for (MavenRepo repo : ss.getRepositories()) {
+			repos.put(repo.getId(), repo.getUrl());
+		}
+		for (ArtifactInfo art : ss.getClassPath().getArtifacts()) {
+			deps.put(art.getCoordinate().toCanonicalForm(), art.getFile().toPath());
+		}
+
+		List<String> comments = source.getTags().collect(Collectors.toList());
 		ClassLoader old = Thread.currentThread().getContextClassLoader();
-		Map<String, byte[]> data = new HashMap<>();
-		List<Map.Entry<String, String>> repos = null;
-		List<Map.Entry<String, Path>> deps = null;
-		Path nativeImage = null;
-		String mainClass = null;
-		List<String> javaArgs = null;
 		PrintStream oldout = System.out;
 		try {
-			// TODO: should we add new properties to the integration method?
-			if (source.getResourceRef().getFile() != null) {
-				System.setProperty("jbang.source", source.getResourceRef().getFile().getAbsolutePath());
-			}
+			URLClassLoader integrationCl = getClassLoader(deps);
 			Thread.currentThread().setContextClassLoader(integrationCl);
 			Set<String> classNames = loadIntegrationClassNames(integrationCl);
 			for (String className : classNames) {
-				if (repos == null) {
-					repos = repositories.stream()
-										.map(s -> new AbstractMap.SimpleEntry<>(s.getId(), s.getUrl()))
-										.collect(Collectors.toList());
-				}
-				if (deps == null) {
-					deps = artifacts.stream()
-									.map(s -> new AbstractMap.SimpleEntry<>(s.getCoordinate().toCanonicalForm(),
-											s.getFile().toPath()))
-									.collect(Collectors.toList());
-				}
-				Class<?> clazz = Class.forName(className, true, integrationCl);
-				Method method = clazz.getDeclaredMethod("postBuild", Path.class, Path.class, List.class, List.class,
-						List.class,
-						boolean.class);
-				Util.infoMsg("Post build with " + className);
-
-				if (Util.isVerbose()) {
-					System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.err)));
-				} else {
-					System.setOut(new PrintStream(new OutputStream() {
-						public void write(int b) {
-							// DO NOTHING
-							// TODO: capture it for later print if error
-						}
-					}));
-				}
-
-				@SuppressWarnings("unchecked")
-				Map<String, Object> integrationResult = (Map<String, Object>) method.invoke(null, tmpJarDir, pomPath,
-						repos, deps, comments, nativeRequested);
-				@SuppressWarnings("unchecked")
-				Map<String, byte[]> ret = (Map<String, byte[]>) integrationResult.get(FILES);
-				if (ret != null) {
-					data.putAll(ret);
-				}
-				Path image = (Path) integrationResult.get(NATIVE_IMAGE);
-				if (image != null) {
-					nativeImage = image;
-				}
-				String mc = (String) integrationResult.get(MAIN_CLASS);
-				if (mc != null) {
-					mainClass = mc;
-				}
-				@SuppressWarnings("unchecked")
-				List<String> ja = (List<String>) integrationResult.get(JAVA_ARGS);
-				if (ja != null) {
-					javaArgs = ja;
-				}
-			}
-			for (Map.Entry<String, byte[]> entry : data.entrySet()) {
-				Path target = tmpJarDir.resolve(entry.getKey());
-				Files.createDirectories(target.getParent());
-				try (OutputStream out = Files.newOutputStream(target)) {
-					out.write(entry.getValue());
-				}
+				Path srcPath = (source.getResourceRef().getFile() != null)
+						? source.getResourceRef().getFile().toPath().toAbsolutePath()
+						: null;
+				IntegrationInput input = new IntegrationInput(className, srcPath, tmpJarDir, pomPath, repos, deps,
+						comments,
+						nativeRequested);
+				IntegrationResult ir = runIntegration(integrationCl, input);
+				result = result.merged(ir);
 			}
 		} catch (ClassNotFoundException e) {
 			throw new ExitException(EXIT_UNEXPECTED_STATE, "Unable to load integration class", e);
@@ -150,8 +89,19 @@ public class IntegrationManager {
 			Thread.currentThread().setContextClassLoader(old);
 			System.setOut(oldout);
 		}
-		return new IntegrationResult(nativeImage, mainClass, javaArgs);
+		return result;
+	}
 
+	@Nonnull
+	private static URLClassLoader getClassLoader(Map<String, Path> deps) {
+		URL[] urls = deps.values().stream().map(path -> {
+			try {
+				return path.toUri().toURL();
+			} catch (MalformedURLException e) {
+				throw new RuntimeException(e);
+			}
+		}).toArray(URL[]::new);
+		return new URLClassLoader(urls);
 	}
 
 	private static Set<String> loadIntegrationClassNames(URLClassLoader integrationCl) throws IOException {
@@ -180,4 +130,67 @@ public class IntegrationManager {
 		return classNames;
 	}
 
+	private static IntegrationResult runIntegration(URLClassLoader integrationCl, IntegrationInput input)
+			throws Exception {
+		Util.infoMsg("Post build with " + input.integrationClassName);
+
+		if (input.source != null) {
+			// TODO: should we add new properties to the integration method?
+			System.setProperty("jbang.source", input.source.toString());
+		}
+
+		Class<?> clazz = Class.forName(input.integrationClassName, true, integrationCl);
+		Method method = clazz.getDeclaredMethod("postBuild", Path.class, Path.class, List.class, List.class,
+				List.class, boolean.class);
+
+		if (Util.isVerbose()) {
+			System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.err)));
+		} else {
+			System.setOut(new PrintStream(new OutputStream() {
+				public void write(int b) {
+					// DO NOTHING
+					// TODO: capture it for later print if error
+				}
+			}));
+		}
+
+		@SuppressWarnings("unchecked")
+		Map<String, Object> result = (Map<String, Object>) method.invoke(null, input.classes, input.pom,
+				mapToList(input.repositories), mapToList(input.dependencies), input.comments, input.nativeRequested);
+
+		@SuppressWarnings("unchecked")
+		Map<String, byte[]> files = (Map<String, byte[]>) result.get(FILES);
+		if (files != null) {
+			for (Map.Entry<String, byte[]> entry : files.entrySet()) {
+				Path target = input.classes.resolve(entry.getKey());
+				Files.createDirectories(target.getParent());
+				try (OutputStream out = Files.newOutputStream(target)) {
+					out.write(entry.getValue());
+				}
+			}
+		}
+
+		Path nativeImage = null;
+		String mainClass = null;
+		List<String> javaArgs = null;
+		Path image = (Path) result.get(NATIVE_IMAGE);
+		if (image != null) {
+			nativeImage = image;
+		}
+		String mc = (String) result.get(MAIN_CLASS);
+		if (mc != null) {
+			mainClass = mc;
+		}
+		@SuppressWarnings("unchecked")
+		List<String> ja = (List<String>) result.get(JAVA_ARGS);
+		if (ja != null) {
+			javaArgs = ja;
+		}
+
+		return new IntegrationResult(nativeImage, mainClass, javaArgs);
+	}
+
+	private static <K, V> List<Map.Entry<K, V>> mapToList(Map<K, V> map) {
+		return new ArrayList<>(map.entrySet());
+	}
 }
