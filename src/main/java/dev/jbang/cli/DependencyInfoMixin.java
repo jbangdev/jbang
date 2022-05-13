1a45a508f81d701df93c057cd6d565bdb2140142
Max Rydahl Andersen <max@xam.dk>
2022-05-13 07:40:28 +0200

    feat: improve maven repo shorthand and logging (#1360)
    `--verbose` now print list of active maven repositories used for resolution.
    
    `--deps` now work as comma separated list
    
    spring-snapshot,spring-milestone,s01sonatype-snapshots and sontatype-snapshots now
    available as short for maven repositories.

--- src/main/java/dev/jbang/cli/DependencyInfoMixin.java
+++ src/main/java/dev/jbang/cli/DependencyInfoMixin.java
@@ -11,7 +11,8 @@ public class DependencyInfoMixin {
 	@CommandLine.Option(names = {
 			"--deps" }, converter = CommaSeparatedConverter.class, description = "Add additional dependencies (Use commas to separate them).")
 	List<String> dependencies;
-	@CommandLine.Option(names = { "--repos" }, description = "Add additional repositories.")
+	@CommandLine.Option(names = {
+			"--repos" }, converter = CommaSeparatedConverter.class, description = "Add additional repositories.")
 	List<String> repositories;
 	@CommandLine.Option(names = { "--cp", "--class-path" }, description = "Add class path entries.")
 	List<String> classpaths;
