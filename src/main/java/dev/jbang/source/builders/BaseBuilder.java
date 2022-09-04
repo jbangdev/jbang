db0fcd6f244733d78be6354396024344d2578c4f
Max Rydahl Andersen <max.andersen@gmail.com>
2022-09-04 07:59:21 +0200

    force build

--- src/main/java/dev/jbang/source/builders/BaseBuilder.java
+++ src/main/java/dev/jbang/source/builders/BaseBuilder.java
@@ -133,7 +133,7 @@ public abstract class BaseBuilder implements Builder {
 		if (!Util.isBlankString(path)) {
 			optionList.addAll(Arrays.asList("-classpath", escapeOSArgument(path, shell)));
 		}
-		optionList.addAll(Arrays.asList("-d", esacpeOSArgument(compileDir.toAbsolutePath().toString(), shell)));
+		optionList.addAll(Arrays.asList("-d", escapeOSArgument(compileDir.toAbsolutePath().toString(), shell)));
 
 		// add source files to compile
 		optionList.addAll(prj	.getMainSourceSet()
