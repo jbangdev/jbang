192972e0518ed60563eff3e7829542a09dbdae60
Tako Schotanus <tako@codejive.org>
2022-10-26 00:02:54 +0200

    fix: now actually passing native options to `nativeimage`

--- itests/helloworld.java
+++ itests/helloworld.java
@@ -1,5 +1,6 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 // //DEPS <dependency1> <dependency2>
+//NATIVE_OPTIONS -O1
 //RUNTIME_OPTIONS -Dfoo=bar "-Dbar=aap noot mies"
 //MANIFEST foo bar=baz baz=${bazprop:nada}
 
