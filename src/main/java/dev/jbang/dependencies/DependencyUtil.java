4910bb7b847d33c28f9b3e2edcb28776edb5ecea
Tako Schotanus <tako@codejive.org>
2023-06-28 20:33:13 +0200

    fix: GAV patterns are now more strict
    They were too lenient and would match too many invalid coordinates.

--- src/main/java/dev/jbang/dependencies/DependencyUtil.java
+++ src/main/java/dev/jbang/dependencies/DependencyUtil.java
@@ -42,10 +42,10 @@ public class DependencyUtil {
 	}
 
 	public static final Pattern fullGavPattern = Pattern.compile(
-			"^(?<groupid>[^:]*):(?<artifactid>[^:]*):(?<version>[^:@]*)(:(?<classifier>[^@]*))?(@(?<type>.*))?$");
+			"^(?<groupid>[a-z0-9_.-]*):(?<artifactid>[a-z0-9_.-]*):(?<version>[^:@]*)(:(?<classifier>[^@]*))?(@(?<type>.*))?$");
 
 	public static final Pattern lenientGavPattern = Pattern.compile(
-			"^(?<groupid>[^:]*):(?<artifactid>[^:]*)(:(?<version>[^:@]*)(:(?<classifier>[^@]*))?)?(@(?<type>.*))?$");
+			"^(?<groupid>[a-z0-9_.-]*):(?<artifactid>[a-z0-9_.-]*)(:(?<version>[^:@]*)(:(?<classifier>[^@]*))?)?(@(?<type>.*))?$");
 
 	private DependencyUtil() {
 	}
