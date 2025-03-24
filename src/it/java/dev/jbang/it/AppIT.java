5cc1ed479f625e7750a95b2949ee05b588cbca00
Max Rydahl Andersen <max@xam.dk>
2025-03-24 23:28:54 +0100

    chore: reimplement template feature

--- src/it/java/dev/jbang/it/AppIT.java
+++ src/it/java/dev/jbang/it/AppIT.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledOnOs;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
+
+import io.qameta.allure.Description;
 public class AppIT extends BaseIT {
 
 
@@ -15,6 +17,7 @@ public class AppIT extends BaseIT {
     // Then match out == "0:foo *\n"
     @Test
     @DisabledOnOs(OS.WINDOWS)
+    @Description("check quotes are kept when wrapped with quotes")
     public void shouldKeepQuotes() {
         assertThat(shell("jbang app install --force --name jbang-itest-app-quote echo.java")).succeeded();
         assertThat(shell("$JBANG_DIR/bin/jbang-itest-app-quote 'foo *'")).succeeded().outIsExactly("0:foo *\n");
---
+++ src/it/java/dev/jbang/it/TemplateIT.java
@@ -0,0 +1,19 @@
+package dev.jbang.it;
+
+import static dev.jbang.it.CommandResultAssert.assertThat;
+
+import org.junit.jupiter.api.Test;
+public class TemplateIT extends BaseIT {
+
+
+    // Scenario: Removing built-in template
+    // When command('jbang template remove hello')
+    // * match exit == 0
+    // * match err contains "Cannot remove template hello from built-in catalog"
+    @Test
+    public void shouldRemoveBuiltInTemplate() {
+        assertThat(shell("jbang template remove hello")).succeeded().errContains("Cannot remove template hello from built-in catalog");
+    }
+
+
+}
