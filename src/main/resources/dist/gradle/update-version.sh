aca217e3a1d493b4b9ee248163406cf02f628774
Werner Fouché <werner.fouche@gmail.com>
2025-04-07 22:41:59 +0200

    Set Gradle version to 8.9

---
+++ src/main/resources/dist/gradle/update-version.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export GRADLE_VERSION=8.9
+
+mv gradle/wrapper/gradle-wrapper-jar gradle/wrapper/gradle-wrapper.jar
+
+touch build.gradle
+touch settings.gradle
+
+./gradlew wrapper --gradle-version $GRADLE_VERSION
+
+mv gradle/wrapper/gradle-wrapper.jar gradle/wrapper/gradle-wrapper-jar
+
+rm -f -r .gradle build.gradle settings.gradle
+
+tree -a
+
+git status
