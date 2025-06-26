e48841ba899e9b21a0795c7bf447341346fb2709
Werner Fouché <werner.fouche@gmail.com>
2025-06-26 19:42:34 +0200

    Documentation for class ConsoleInputReadTask

--- src/main/java/dev/jbang/util/ConsoleInputReadTask.java
+++ src/main/java/dev/jbang/util/ConsoleInputReadTask.java
@@ -5,6 +5,16 @@ import java.io.InputStream;
 import java.util.Scanner;
 import java.util.concurrent.Callable;
 
+/**
+ * A reader task that is run in a separate thread, using
+ * {@code Executors.newSingleThreadExecutor()}, to read a line of text (String)
+ * from the console. The calling thread will wait for a pre-determined number of
+ * seconds for a result to be returned by the reader task. If the calling thread
+ * times out, it immediately interrupts the reader task signalling that the read
+ * operation should be aborted (in which case {@code null} is returned). Note
+ * that the user must press {@code ENTER} on the keyboard to indicate that a
+ * line of text has been entered. Empty lines of text are supported.
+ */
 public class ConsoleInputReadTask implements Callable<String> {
 	private final Scanner sc;
 
