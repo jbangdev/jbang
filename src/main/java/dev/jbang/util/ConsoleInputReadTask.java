3f061160cdb0062477a168f9135d7d5063882ac7
Werner Fouché <werner.fouche@gmail.com>
2025-06-26 20:03:32 +0200

    Documentation for the Scanner object

--- src/main/java/dev/jbang/util/ConsoleInputReadTask.java
+++ src/main/java/dev/jbang/util/ConsoleInputReadTask.java
@@ -8,14 +8,20 @@ import java.util.concurrent.Callable;
 /**
  * A reader task that is run in a separate thread, using
  * {@code Executors.newSingleThreadExecutor()}, to read a line of text (String)
- * from the console. The calling thread will wait for a pre-determined number of
- * seconds for a result to be returned by the reader task. If the calling thread
- * times out, it immediately interrupts the reader task signalling that the read
- * operation should be aborted (in which case {@code null} is returned). Note
- * that the user must press {@code ENTER} on the keyboard to indicate that a
- * line of text has been entered. Empty lines of text are supported.
+ * from the console. Empty lines are ignored. The calling thread will wait for a
+ * pre-determined number of seconds for a result to be returned by the reader
+ * task. If the calling thread times out, it immediately interrupts the reader
+ * task signalling that the read operation should be aborted (in which case
+ * {@code null} is returned).
  */
 public class ConsoleInputReadTask implements Callable<String> {
+
+	/**
+	 * Input stream state is maintained in the Scanner object across multiple calls
+	 * to the reader task. The scanner object is never closed (do not call
+	 * {@code Scanner.close} as it will close the underlying {@code InputStream}
+	 * potentially closing {@code System.in}.
+	 */
 	private final Scanner sc;
 
 	public ConsoleInputReadTask(InputStream in) {
@@ -26,14 +32,20 @@ public class ConsoleInputReadTask implements Callable<String> {
 		String input;
 		do {
 			try {
-				// wait until we have data to complete a nextLine()
+				// wait until we have enough data to complete a call to nextLine()
 				while (!sc.hasNextLine()) {
+					// Sleep for a brief moment to avoid busy waiting
+					// and allow the reader task to be interrupted if the
+					// calling thread times out
 					Thread.sleep(200);
 				}
+				// read the available line of text from the InputStream
 				input = sc.nextLine();
 			} catch (InterruptedException e) {
+				// the calling thread timed out and interrupted the reader task
 				return null;
 			}
+			// if the {@code input} string is empty, retry by reading the next line
 		} while ("".equals(input));
 		return input;
 	}
