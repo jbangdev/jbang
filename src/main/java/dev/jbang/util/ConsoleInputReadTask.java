60567daabe1c5e02f6aa7f48185c35d6488c725a
Werner Fouché <werner.fouche@gmail.com>
2025-06-26 18:20:50 +0200

    Instantiate a single Scanner object to be re-used during retries.

--- src/main/java/dev/jbang/util/ConsoleInputReadTask.java
+++ src/main/java/dev/jbang/util/ConsoleInputReadTask.java
@@ -6,14 +6,13 @@ import java.util.Scanner;
 import java.util.concurrent.Callable;
 
 public class ConsoleInputReadTask implements Callable<String> {
-	private final InputStream in;
+	private final Scanner sc;
 
 	public ConsoleInputReadTask(InputStream in) {
-		this.in = in;
+		this.sc = new Scanner(in);
 	}
 
 	public String call() throws IOException {
-		Scanner sc = new Scanner(in);
 		String input;
 		do {
 			try {
