f29738b023deb39bac28d21f34d8acff1695f4ed
Tako Schotanus <tako@codejive.org>
2025-06-27 12:55:52 +0200

    chore: absolute //FILES path no longer throw exception
    They are now also treated as "unresolvable".
    Also added test for //FILES with absolute paths

---
+++ itests/abstagsnix.java
@@ -0,0 +1,11 @@
+
+//SOURCES Two.java
+///SOURCES /nested/*.java
+
+//FILES /res/resource.properties renamed.properties=/res/resource.properties
+//FILES META-INF/application.properties=res/resource.properties
+
+public class abstagsnix {
+    public static void main(String... args) {
+    }
+}
---
+++ itests/abstagswin.java
@@ -0,0 +1,11 @@
+
+//SOURCES Two.java
+///SOURCES C:/nested/*.java
+
+//FILES C:/res/resource.properties renamed.properties=C:/res/resource.properties
+//FILES META-INF/application.properties=res/resource.properties
+
+public class abstagswin {
+    public static void main(String... args) {
+    }
+}
