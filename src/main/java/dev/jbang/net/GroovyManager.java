bca0d867f63474cf8c6c25fd493355ff32d2ff7d
Werner Fouché <werner.fouche@gmail.com>
2025-07-05 20:46:55 +0200

    chore(deps): default Groovy version set to 4.0.27 (#2083)
    Co-authored-by: Max Rydahl Andersen <max@xam.dk>

--- src/main/java/dev/jbang/net/GroovyManager.java
+++ src/main/java/dev/jbang/net/GroovyManager.java
@@ -15,7 +15,7 @@ import dev.jbang.util.UnpackUtil;
 import dev.jbang.util.Util;
 
 public class GroovyManager {
-	public static final String DEFAULT_GROOVY_VERSION = "4.0.14";
+	public static final String DEFAULT_GROOVY_VERSION = "4.0.27";
 
 	public static String resolveInGroovyHome(String cmd, String requestedVersion) {
 		Path groovyHome = getGroovy(requestedVersion);
