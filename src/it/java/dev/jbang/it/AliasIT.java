dadff507f1eb77215910cad0640681a4c8477be5
andsel <selva.andre@gmail.com>
2025-08-04 18:00:43 +0200

    Updated alias command

--- src/it/java/dev/jbang/it/AliasIT.java
+++ src/it/java/dev/jbang/it/AliasIT.java
@@ -2,6 +2,7 @@ package dev.jbang.it;
 
 import static dev.jbang.it.CommandResultAssert.assertThat;
 
+import io.qameta.allure.Description;
 import org.junit.jupiter.api.Test;
 
 public class AliasIT extends BaseIT {
@@ -19,6 +20,26 @@ public class AliasIT extends BaseIT {
 		assertThat(shell("jbang alias add -f " + scratch() + " echo.java")).succeeded();
 		assertThat(shell("jbang alias list -f " + scratch())).succeeded().outNotContains("Properties");
 		assertThat(shell("jbang alias remove -f " + scratch() + " echo")).succeeded();
+	}
+
+	// Scenario: check help command is printed when -h is requested
+	// * command('jbang app --help')
+	@Test
+	@Description("Check --help write help on console for top level command")
+	public void shouldPrintHelp() {
+		assertThat(shell("jbang alias --help"))
+				.succeeded()
+				.errContains("Use 'jbang <command> -h' for detailed");
+	}
+
+	// Scenario: check help command is printed when -h is requested
+	// * command('jbang app --help')
+	@Test
+	@Description("Check prints detailed help on missed subcommand argument")
+	public void shouldPrintDetailedHelpOnMissedSubcommand() {
+		assertThat(shell("jbang alias"))
+				.failed()
+				.errContains("Missing required subcommand");
 	}
 
 }
