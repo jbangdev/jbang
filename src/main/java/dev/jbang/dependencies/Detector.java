6e473143b69ce8877485607254c110d6aeef4f23
Tamas Cservenak <tamas@cservenak.net>
2025-08-23 16:56:29 +0200

    Update PR to Nisse 0.6.0
    And simplify things: we use only OS source, so no need
    for "compatibility", just apply naming strategy we need
    and done.

--- src/main/java/dev/jbang/dependencies/Detector.java
+++ src/main/java/dev/jbang/dependencies/Detector.java
@@ -1,18 +1,15 @@
 package dev.jbang.dependencies;
 
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
-import java.util.function.BiFunction;
-
-import dev.jbang.util.Util;
 
 import eu.maveniverse.maven.nisse.core.NisseConfiguration;
-import eu.maveniverse.maven.nisse.core.PropertySource;
-import eu.maveniverse.maven.nisse.core.internal.SimpleNisseConfiguration;
+import eu.maveniverse.maven.nisse.core.NisseManager;
+import eu.maveniverse.maven.nisse.core.PropertyKeyNamingStrategies;
+import eu.maveniverse.maven.nisse.core.simple.SimpleNisseConfiguration;
+import eu.maveniverse.maven.nisse.core.simple.SimpleNisseManager;
 import eu.maveniverse.maven.nisse.source.osdetector.OsDetectorPropertySource;
 
 public class Detector {
@@ -21,54 +18,29 @@ public class Detector {
 		super();
 	}
 
-	private final List<PropertySource> sources = Arrays.asList(new OsDetectorPropertySource());
-
-	public Map<String, String> createProperties(NisseConfiguration configuration) {
-		BiFunction<PropertySource, String, List<String>> propertyKeyNamingStrategy = configuration
-			.propertyKeyNamingStrategy();
-		HashMap<String, String> properties = new HashMap<>();
-		for (PropertySource source : this.sources) {
-			if (configuration.isPropertySourceActive(source)) {
-				source.getProperties(configuration).forEach((key, value) -> {
-					for (String translated : propertyKeyNamingStrategy.apply(source, key)) {
-						properties.put(translated, value);
-					}
-				});
-			}
-		}
-		return properties;
-	}
+	private final NisseManager nisseManager = new SimpleNisseManager(Collections.singletonList(new OsDetectorPropertySource()));
 
 	public void detect(final Properties properties, List<String> classiferWithLikes) {
 
-		// TODO: this is a minimal use of nisse to replace the os-maven-plugin defaults.
-		// in future maybe add more from nisse but for now keep it simple.
-		Properties uPropertiers = new Properties(properties);
-		uPropertiers.put("nisse.compat.osDetector", "true");
-		try {
-			NisseConfiguration configuration = SimpleNisseConfiguration.builder()
-				.withSystemProperties(System.getProperties())
-				.withUserProperties(uPropertiers)
-				.build();
+        NisseConfiguration configuration = SimpleNisseConfiguration.builder()
+            .withSystemProperties(System.getProperties())
+            .withPropertyKeyNamingStrategy(PropertyKeyNamingStrategies.prefixed("os.detected."))
+            .build();
 
-			Map<String, String> props = createProperties(configuration);
+        Map<String, String> props = nisseManager.createProperties(configuration);
 
-			properties.putAll(props);
+        properties.putAll(props);
 
-			// "hack" to expose a property that works with javafx mac classifers
-			String os = properties.getProperty("os.detected.name");
-			if (os.equals("osx")) {
-				os = "mac";
-				if ("aarch64".equals(System.getProperty("os.arch"))) {
-					os = "mac-aarch64";
-				}
-			} else if (os.equals("windows")) {
-				os = "win";
-			}
-			properties.setProperty("os.detected.jfxname", os);
-
-		} catch (IOException e) {
-			Util.warnMsg("Failed to detect OS properties, continuing without them.", e);
-		}
+        // "hack" to expose a property that works with javafx mac classifers
+        String os = properties.getProperty("os.detected.name");
+        if (os.equals("osx")) {
+            os = "mac";
+            if ("aarch64".equals(System.getProperty("os.arch"))) {
+                os = "mac-aarch64";
+            }
+        } else if (os.equals("windows")) {
+            os = "win";
+        }
+        properties.setProperty("os.detected.jfxname", os);
 	}
 }
