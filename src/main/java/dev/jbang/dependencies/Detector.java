f45f2c8b7119acea4d7a619c91483e198c5aeec0
Tamas Cservenak <tamas@cservenak.net>
2025-08-25 09:57:30 +0200

    Reformat

--- src/main/java/dev/jbang/dependencies/Detector.java
+++ src/main/java/dev/jbang/dependencies/Detector.java
@@ -13,29 +13,30 @@ import eu.maveniverse.maven.nisse.core.simple.SimpleNisseManager;
 import eu.maveniverse.maven.nisse.source.osdetector.OsDetectorPropertySource;
 
 public class Detector {
-	private final NisseManager nisseManager = new SimpleNisseManager(Collections.singletonList(new OsDetectorPropertySource()));
+	private final NisseManager nisseManager = new SimpleNisseManager(
+			Collections.singletonList(new OsDetectorPropertySource()));
 
 	public void detect(Properties properties, List<String> classiferWithLikes) {
 
-        NisseConfiguration configuration = SimpleNisseConfiguration.builder()
-            .withSystemProperties(System.getProperties())
-            .withPropertyKeyNamingStrategy(PropertyKeyNamingStrategies.prefixed("os.detected."))
-            .build();
+		NisseConfiguration configuration = SimpleNisseConfiguration.builder()
+			.withSystemProperties(System.getProperties())
+			.withPropertyKeyNamingStrategy(PropertyKeyNamingStrategies.prefixed("os.detected."))
+			.build();
 
-        Map<String, String> props = nisseManager.createProperties(configuration);
+		Map<String, String> props = nisseManager.createProperties(configuration);
 
-        properties.putAll(props);
+		properties.putAll(props);
 
-        // "hack" to expose a property that works with javafx mac classifers
-        String os = properties.getProperty("os.detected.name");
-        if (os.equals("osx")) {
-            os = "mac";
-            if ("aarch64".equals(System.getProperty("os.arch"))) {
-                os = "mac-aarch64";
-            }
-        } else if (os.equals("windows")) {
-            os = "win";
-        }
-        properties.setProperty("os.detected.jfxname", os);
+		// "hack" to expose a property that works with javafx mac classifers
+		String os = properties.getProperty("os.detected.name");
+		if (os.equals("osx")) {
+			os = "mac";
+			if ("aarch64".equals(System.getProperty("os.arch"))) {
+				os = "mac-aarch64";
+			}
+		} else if (os.equals("windows")) {
+			os = "win";
+		}
+		properties.setProperty("os.detected.jfxname", os);
 	}
 }
