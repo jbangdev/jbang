52ecb09f67eaf735732fbd7adf346470ba1142b4
Werner Fouché <werner.fouche@gmail.com>
2025-09-12 10:42:09 +0200

    fix: add script to format all Java example programs

---
+++ docs/modules/ROOT/examples/formatExamples.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export GJF_JAR=com.google.googlejavaformat:google-java-format:1.28.0
+
+find . -name "*.java" | while read -r file; do
+    echo "Processing file: $file"
+    jbang run $GJF_JAR -r $file
+    jbang run FixCommentSpacing.java $file
+done
