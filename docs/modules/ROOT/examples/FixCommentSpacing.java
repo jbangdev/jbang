b146326982791cb1a85d2cb9d74b6da6a6f09605
Werner Fouché <werner.fouche@gmail.com>
2025-09-12 11:06:16 +0200

    fix: testing formatting with four spaces

--- docs/modules/ROOT/examples/FixCommentSpacing.java
+++ docs/modules/ROOT/examples/FixCommentSpacing.java
@@ -4,28 +4,30 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class FixCommentSpacing {
-  public static void main(String[] args) throws IOException {
-    if (args.length != 1) {
-      System.err.println("Usage: jbang FixCommentSpacing.java <filename>");
-      System.exit(1);
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.err.println("Usage: jbang FixCommentSpacing.java <filename>");
+            System.exit(1);
+        }
+
+        Path path = Paths.get(args[0]);
+        List<String> lines = Files.readAllLines(path);
+
+        List<String> fixedLines =
+                lines.stream()
+                        .map(FixCommentSpacing::fixCommentSpacing)
+                        .collect(Collectors.toList());
+
+        Files.write(path, fixedLines);
     }
 
-    Path path = Paths.get(args[0]);
-    List<String> lines = Files.readAllLines(path);
-
-    List<String> fixedLines =
-        lines.stream().map(FixCommentSpacing::fixCommentSpacing).collect(Collectors.toList());
-
-    Files.write(path, fixedLines);
-  }
-
-  private static String fixCommentSpacing(String line) {
-    if (line.startsWith("/// ")) {
-      return line.replaceFirst("///\\s+", "///");
-    } else if (line.startsWith("// ")) {
-      return line.replaceFirst("//\\s+", "//");
-    } else {
-      return line;
+    private static String fixCommentSpacing(String line) {
+        if (line.startsWith("/// ")) {
+            return line.replaceFirst("///\\s+", "///");
+        } else if (line.startsWith("// ")) {
+            return line.replaceFirst("//\\s+", "//");
+        } else {
+            return line;
+        }
     }
-  }
 }
--- docs/modules/ROOT/examples/formatExamples.sh
+++ docs/modules/ROOT/examples/formatExamples.sh
@@ -4,6 +4,6 @@ export GJF_JAR=com.google.googlejavaformat:google-java-format:1.28.0
 
 find . -name "*.java" | while read -r file; do
     echo "Processing file: $file"
-    jbang run $GJF_JAR -r $file
+    jbang run $GJF_JAR --aosp -r $file
     jbang run FixCommentSpacing.java $file
 done
