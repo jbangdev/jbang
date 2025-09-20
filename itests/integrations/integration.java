2f4298bbdf11105acd2e9f48fb22f805fb3ee59c
Tako Schotanus <tako@codejive.org>
2025-09-20 17:13:25 +0200

    test: added IT test for integrations
    Also included a minor fixes for setting the main class from integrations
    and correctly closing integration class loaders.

---
+++ itests/integrations/integration.java
@@ -0,0 +1,51 @@
+
+//FILES META-INF/jbang-integration.list=jbang-integration.list
+
+import java.nio.file.Path;
+import java.util.*;
+
+public class integration {
+    /**
+     *
+     * @param temporaryJar temporary JAR file path
+     * @param pomFile location of pom.xml representing the projects dependencies
+     * @param repositories list of the used repositories
+     * @param dependencies list of GAV to Path of artifact/classpath dependencies
+     * @param comments comments from the source file
+     * @param nativeImage true if --native been requested
+     * @return Map<String, Object> map of returns; special keys are "native-image" which is a and "files" to
+     *          return native-image to be run and list of files to get written to the output directory.
+     *
+     */
+    public static Map<String, Object> postBuild(
+            Path temporaryJar,
+            Path pomFile,
+            List<Map.Entry<String, String>> repositories,
+            List<Map.Entry<String, Path>> dependencies,
+            List<String> comments,
+    boolean nativeImage) {
+        System.out.println("Integration called!");
+        System.out.println("TMPJAR: " + temporaryJar);
+        System.out.println("POM: " + pomFile);
+        for (Map.Entry<String, String> r : repositories) {
+            System.out.println("REPO: " + r.getKey() + " -> " + r.getValue());
+        }
+        for (Map.Entry<String, Path> d : dependencies) {
+            System.out.println("DEP: " + d.getKey() + " -> " + d.getValue());
+        }
+        for (String c : comments) {
+            System.out.println("COMMENT: " + c);
+        }
+
+        String myTest = comments.stream()
+                .filter(s -> s.startsWith("//MY:TEST "))
+                .findFirst()
+                .map(s -> s.substring("//MY:TEST ".length()))
+                .orElse("default");
+        Map<String, Object> result = new HashMap<>();
+        result.put("java-args", Arrays.asList("-Dbar=" + myTest));
+        result.put("main-class", "altmain");
+        return result;
+    }
+
+}
---
+++ itests/integrations/main.java
@@ -0,0 +1,21 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+
+//DEPS integration.java
+//RUNTIME_OPTIONS -Dfoo=fubar
+//MY:TEST aap
+
+public class main {
+
+    public static void main(String... args) {
+        System.out.println("Wrong main!!!");
+    }
+}
+
+class altmain {
+
+    public static void main(String... args) {
+        System.out.println("Hello World!");
+        System.out.println("foo: " + System.getProperty("foo"));
+        System.out.println("bar: " + System.getProperty("bar"));
+    }
+}
