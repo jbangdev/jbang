4c5b75c026bbec6bf93b9eb47f3ef6657bb22f44
Tako Schotanus <tako@codejive.org>
2025-09-23 18:31:10 +0200

    test: added test for //DEPS quoting

--- src/test/java/dev/jbang/source/parser/TestDirectives.java
+++ src/test/java/dev/jbang/source/parser/TestDirectives.java
@@ -3,6 +3,7 @@ package dev.jbang.source.parser;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasSize;
 
 import java.util.List;
 
@@ -18,6 +19,7 @@ public class TestDirectives {
 				"//DEPS foo:bar, abc:DEF:123, https://github.com/jbangdev/jbang, something", null);
 
 		List<String> deps = tr.binaryDependencies();
+		assertThat(deps, hasSize(3));
 		assertThat(deps, containsInAnyOrder("foo:bar", "abc:DEF:123", "https://github.com/jbangdev/jbang"));
 
 		List<String> subs = tr.sourceDependencies();
@@ -30,10 +32,24 @@ public class TestDirectives {
 				"//DEPS foo:bar, abc:DEF:123, \thttps://github.com/jbangdev/jbang \tsomething\t ", null);
 
 		List<String> deps = tr.binaryDependencies();
+		assertThat(deps, hasSize(3));
 		assertThat(deps, containsInAnyOrder("foo:bar", "abc:DEF:123", "https://github.com/jbangdev/jbang"));
 
 		List<String> subs = tr.sourceDependencies();
 		assertThat(subs, containsInAnyOrder("something"));
+	}
+
+	@Test
+	void testExtractDependenciesQuoted() {
+		Directives tr = new Directives.Extended(
+				"//DEPS abc:DEF:123, 'ch.qos.reload4j:reload4j:[1.2.18,1.2.19)', 'some thing'", null);
+
+		List<String> deps = tr.binaryDependencies();
+		assertThat(deps, hasSize(2));
+		assertThat(deps, containsInAnyOrder("abc:DEF:123", "ch.qos.reload4j:reload4j:[1.2.18,1.2.19)"));
+
+		List<String> subs = tr.sourceDependencies();
+		assertThat(subs, containsInAnyOrder("some thing"));
 	}
 
 	@Test
