5b28706167310a2b3a2b5f2f7712ba30ad98ecf3
Werner Fouché <werner.fouche@gmail.com>
2025-12-06 14:46:36 +0200

    fix: remove leading spaces

--- src/main/scripts/jbang.cmd
+++ src/main/scripts/jbang.cmd
@@ -4,11 +4,11 @@ setlocal
 
 :: Save the current code page
 :: The 'chcp' command outputs "Active code page: 437"
-:: We use a FOR loop to split by output by ":" and grab the second part (the number)
+:: We use a FOR loop to split the output by ":" and grab the second part (the number)
 for /f "tokens=2 delims=:" %%a in ('chcp') do set "_JBANG_CMD_OriginalCP=%%a"
 
 :: Clean up the variable (remove leading spaces)
-set "_JBANG_CMD_OriginalCP=%_OriginalCP: =%"
+set "_JBANG_CMD_OriginalCP=%_JBANG_CMD_OriginalCP: =%"
 
 :: Set code page to UTF-8 (65001)
 chcp 65001 > nul
