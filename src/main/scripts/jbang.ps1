33b5e7b49f016094769f789fe181fb60c54ffd04
Werner Fouché <werner.fouche@gmail.com>
2025-12-09 12:39:59 +0200

    fix: removed comment in jbang.ps1

--- src/main/scripts/jbang.ps1
+++ src/main/scripts/jbang.ps1
@@ -203,7 +203,6 @@ if (-not $binaryPath) {
   }
 }
 
-# Set console output to UTF-8
 if ([System.Environment]::GetEnvironmentVariable('JBANG_WIN_ENABLE_UTF8') -eq 'true') {
   $OutputEncoding = [Console]::InputEncoding = [Console]::OutputEncoding = [System.Text.UTF8Encoding]::new($false)
 }
