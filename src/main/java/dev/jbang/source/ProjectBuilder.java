db7de3c00a38ae2e4c25c0d13564d3d2b47f25ba
Werner Fouché <werner.fouche@gmail.com>
2026-01-09 17:49:13 +0200

    Declare version variable where it is used

--- src/main/java/dev/jbang/source/ProjectBuilder.java
+++ src/main/java/dev/jbang/source/ProjectBuilder.java
@@ -266,10 +266,9 @@ public class ProjectBuilder {
 	private ResourceRef resolveChecked(ResourceResolver resolver, String _resource) {
 		Util.verboseMsg("Resolving resource ref: " + _resource);
 		String resource;
-		String version = "";
 		int idx = _resource.indexOf("#");
 		if (idx != -1) {
-			version = _resource.substring(idx + 1);
+			String version = _resource.substring(idx + 1);
 			resource = _resource.substring(0, idx);
 			System.setProperty("jbang.alias.version", version);
 		} else {
